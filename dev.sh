553fe8bc1c6f1cb84103d66f24e0ef485f59d9e7
jolow99 <jolow999@gmail.com>
2026-02-17 16:43:53 +0200

    Update local dev.sh

--- dev.sh
+++ dev.sh
@@ -5,6 +5,10 @@
 
 trap 'kill 0; exit' SIGINT SIGTERM
 
+# Kill any existing processes on our ports
+lsof -ti :3000 | xargs kill -9 2>/dev/null
+lsof -ti :8000 | xargs kill -9 2>/dev/null
+
 # Frontend
 (cd frontend && npm run dev) &
 
